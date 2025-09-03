b9982d0b3ee03b0f66373cd6c8fa819a74a8a287
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-09-03 11:12:58 -0600

    primera parte de la calculadora suma

--- interfaz.py
+++ interfaz.py
@@ -4,13 +4,16 @@ ventana = tk.Tk()
 ventana.title("CALCULADORA")
 ventana.geometry("300x200")
 
-etiqueta = tk.Label(ventana, text="Numero 1:")
+etiqueta = tk.Label(ventana, text="CALCULADORA", font=("Arial", 22, "bold"), fg="red")
+etiqueta.pack(pady=5)
+
+etiqueta = tk.Label(ventana, text="Ingresa el primer número:")
 etiqueta.pack(pady=5)
 
 entrada1 = tk.Entry(ventana)
 entrada1.pack(pady=5)
 
-etiqueta = tk.Label(ventana, text="Numero 2:")
+etiqueta = tk.Label(ventana, text="Ingresa el segundo número:")
 etiqueta.pack(pady=5)
 
 entrada2 = tk.Entry(ventana)
@@ -26,7 +29,16 @@ def sumar():
         result = numero1 + numero2
         resultado.config(text=f"Resultado: {result}")
     except ValueError:
-        resultado.config(text=f"Ingresa solo números")
+        resultado.config(text=f"Ingresa solo numeros.")
+
+def restar():
+    try:
+        numero1 = float(entrada1.get())
+        numero2 = float(entrada2.get())
+        result = numero1 - numero2
+        resultado.config(text=f"Resultado: {result}")
+    except ValueError:
+        resultado.config(text=f"Ingresa solo numeros.")
 
 def limpiar():
     entrada1.delete(0, tk.END)
@@ -35,6 +47,9 @@ def limpiar():
 
 boton_sumar = tk.Button(ventana, text="SUMAR", command=sumar)
 boton_sumar.pack(pady=5)
+
+boton_restar = tk.Button(ventana, text="RESTAR", command=restar)
+boton_restar.pack(pady=5)
 
 botono_limpiar = tk.Button(ventana, text="LIMPIAR", command=limpiar)
 botono_limpiar.pack(pady=5)
